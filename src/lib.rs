c2ea1d0a29dd92cc8899da245ebbefd43be870e8
David Justice <david@devigned.com>
2015-03-26 08:29:16 -0700

    kind of works

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
-#[test]
-fn it_works() {
+#![feature(plugin)]
+#![feature(custom_attribute)]
+#![plugin(postgres_extension)]
+#![feature(libc)]
+#[macro_use] extern crate postgres_extension;
+extern crate vault;
+extern crate libc;
+
+use libc::{c_int};
+
+pg_module!(version: 90400);
+
+#[pg_export]
+pub fn is_zero(a: c_int) -> c_int {
+    if a == 0 {
+        5
+    } else {
+        41
+    }
 }
